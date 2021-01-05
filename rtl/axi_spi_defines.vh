e973d057060fd5eacdc4c42f3a2857fc055d9706
m4j0rt0m <major.tom.spaceworm@gmail.com>
2021-01-05 19:11:05 +0100

    Update info-headers for every rtl module

--- rtl/axi_spi_defines.vh
+++ rtl/axi_spi_defines.vh
@@ -1,3 +1,20 @@
+/* -------------------------------------------------------------------------------
+ * Project        : AXI-lite SPI IP Core
+ * File           : axi_spi_defines.vh
+ * Description    : AXI4-Lite SPI parameters defines
+ * Organization   : BSC; CIC-IPN
+ * Author(s)      : Abraham J. Ruiz R. (aruiz) (https://github.com/m4j0rt0m)
+ * Email(s)       : abraham.ruiz@bsc.es; abraham.j.ruiz.r@gmail.com
+ * References     :
+ * -------------------------------------------------------------------------------
+ * Revision History
+ *  Revision   | Author      | Description
+ *  1.0        | aruiz       | First version
+ *  2.0        | aruiz       | Added asynchronous reset and soft reset
+ *  2.1        | aruiz       | Code refactoring and added Ratio Clock Gen
+ *  3.0        | aruiz       | Sync'd a main fixed clock and a variable faster
+ *             |             | clock for the axi write/read transactions
+ * -----------------------------------------------------------------------------*/
 
   `ifndef _AXI_SPI_DEFINES_H_
   `define _AXI_SPI_DEFINES_H_
--- rtl/spi_exch_byte.v
+++ rtl/spi_exch_byte.v
@@ -1,9 +1,21 @@
-/*
- * Author:        Abraham J. Ruiz R. (https://github.com/m4j0rt0m)
- * File:          spi_exch_byte.v
- * Description:   SPI mosi/miso data exchange control
- * Organizations: BSC; CIC-IPN
- */
+/* -------------------------------------------------------------------------------
+ * Project        : AXI-lite SPI IP Core
+ * File           : spi_exch_byte.v
+ * Description    : SPI mosi/miso data exchange control
+ * Organization   : BSC; CIC-IPN
+ * Author(s)      : Abraham J. Ruiz R. (aruiz) (https://github.com/m4j0rt0m)
+ * Email(s)       : abraham.ruiz@bsc.es; abraham.j.ruiz.r@gmail.com
+ * References     :
+ * -------------------------------------------------------------------------------
+ * Revision History
+ *  Revision   | Author      | Description
+ *  1.0        | aruiz       | First version
+ *  2.0        | aruiz       | Added asynchronous reset and soft reset
+ *  2.1        | aruiz       | Code refactoring and added Ratio Clock Gen
+ *  3.0        | aruiz       | Sync'd a main fixed clock and a variable faster
+ *             |             | clock for the axi write/read transactions
+ * -----------------------------------------------------------------------------*/
+
 module spi_exch_byte
 # (
     parameter BYTE  = 8
